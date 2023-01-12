f8518ea7139e3a18be0ef3bd3360110566c56404
Sergey Chebotarev <d7d1cd@gmail.com>
2023-01-12 15:27:01 +0300

    Создание файлов для исходного кода класса по работе с файлом базы данных, настройка сборки

---
+++ include/ifile/data_base.h
@@ -0,0 +1,36 @@
+/*
+     _____ __    _____
+    |     |  |  |_   _|  C++ Object Libraries for IBM i
+    |  |  |  |__ _| |_   File:   IFILE/data_base.h
+    |_____|_____|_____|  Author: Sergey Chebotarev
+
+*/
+
+#ifndef OLI_IFILE_DATA_BASE_H
+#define OLI_IFILE_DATA_BASE_H
+
+#include "IFILE/rfile.h"
+
+IBMI_NAMESPACE_BEGIN ///////////////////////////////////////////////////////////////////////////////////////////////////
+FILE_NAMESPACE_BEGIN
+
+
+
+
+
+//**********************************************************************************************************************
+template <typename R, typename K = void>
+class data_base
+{
+};
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+FILE_NAMESPACE_END
+IBMI_NAMESPACE_END /////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#endif // OLI_IFILE_DATA_BASE_H
+
---
+++ test/data_base$.cpp
@@ -0,0 +1,17 @@
+#include "IFILE/data_base.h"
+#include "GOOGLE/gtest.h"
+
+
+
+//**********************************************************************************************************************
+IBMI_NAMESPACE_BEGIN
+FILE_NAMESPACE_BEGIN
+
+TEST(data_base, constructor)
+{
+  data_base<int> db;
+}
+
+FILE_NAMESPACE_END
+IBMI_NAMESPACE_END
+
